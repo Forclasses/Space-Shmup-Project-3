99f72970c1d1f790d243077ec082ddf1fbe2676c
Forclasses <brucec22180@gmail.com>
2025-03-24 17:33:22 -0500

    Something is worng with deleting stuff
    The off-screen delet is not acting right.. added tempory fix in enemy.. hope it works

--- Assets/__Scripts/BoundsCheck.cs
+++ Assets/__Scripts/BoundsCheck.cs
@@ -5,14 +5,32 @@ using UnityEngine;
 
 public class BoundsCheck : MonoBehaviour
 {
+        [System.Flags]
+    public enum eScreenLocs {
+        onScreen = 0,
+        offRight = 1,
+        offLeft = 2,
+
+        offUp = 4,
+        offDown = 8
+    }
+
+
     public enum eType { center, insert, outset };
+
+
 
     [Header("Inscribed")]
     public eType boundsType = eType.center;
     public float radius = 1f;
+    public bool keepOnScreen = true;
+
 
 
     [Header("Dynamic")]
+
+    public eScreenLocs screenLocs = eScreenLocs.onScreen;
+   
     public float camWidth;
     public float camHeight;
 
@@ -33,23 +51,47 @@ public class BoundsCheck : MonoBehaviour
 
 
         Vector3 pos = transform.position;
+        screenLocs = eScreenLocs.onScreen;
+        
 
-        if (pos.x > camWidth + checkRadius){
+        if (pos.x > (camWidth + checkRadius)){
             pos.x = camWidth + checkRadius;
+            screenLocs |= eScreenLocs.offRight;
+            
         }
 
-        if (pos.x < -camWidth - checkRadius){
+        if (pos.x < (-camWidth - checkRadius)){
             pos.x = -camWidth - checkRadius;
+            screenLocs |= eScreenLocs.offLeft;
+            
         }
 
-        if (pos.x > camHeight + checkRadius){
+        if (pos.x > (camHeight + checkRadius)){
             pos.x = camHeight + checkRadius;
+            screenLocs |= eScreenLocs.offUp;
+           
         }
 
-        if (pos.x < -camHeight - checkRadius){
+        if (pos.x < (-camHeight - checkRadius)){
             pos.x = -camHeight - checkRadius;
+            screenLocs |= eScreenLocs.offDown;
+            
+        }
+        
+        if( keepOnScreen && !isOnScreen ){
+            transform.position = pos;
+            screenLocs = eScreenLocs.onScreen;
+            
         }
 
-        transform.position = pos;
+    }
+
+    public bool isOnScreen {
+        get{ return (screenLocs == eScreenLocs.onScreen);}
+    }
+
+    public bool LocIs( eScreenLocs checkLoc ){
+        if ( checkLoc == eScreenLocs.onScreen) return isOnScreen;
+        return ( ( screenLocs & checkLoc) == checkLoc);
     }
 }
--- Assets/__Scripts/Enemy.cs
+++ Assets/__Scripts/Enemy.cs
@@ -6,11 +6,20 @@ using UnityEngine.Scripting.APIUpdating;
 
 public class Enemy : MonoBehaviour
 {
+
+
     [Header("Inscribed")]
     public float speed = 10f;
     public float fireRate = 0.3f;
     public float health = 10;
     public int score = 100;
+
+    private BoundsCheck bndCheck;
+
+    void Awake()
+    {
+        bndCheck = GetComponent<BoundsCheck>();
+    }
 
     public Vector3 pos {
         get {
@@ -26,6 +35,19 @@ public class Enemy : MonoBehaviour
     void Update()
     {
         Move();
+        //this is not wokring
+
+        if( bndCheck.LocIs( BoundsCheck.eScreenLocs.offDown )){
+
+            print("Is getting singal");
+            Destroy( gameObject );
+        }
+        //this works well enough for now something is off with my bounds check and locis..
+
+        if (pos.y < -50){
+            Destroy( gameObject );
+
+        }
     }
 
     public virtual void Move(){
