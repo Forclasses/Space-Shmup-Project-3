521b25db10184515c04f4acad4a710139819ec1e
Forclasses <brucec22180@gmail.com>
2025-03-29 15:35:54 -0500

    Hero Projectiole added and slef delets

--- Assets/__Scripts/Hero.cs
+++ Assets/__Scripts/Hero.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Net.Http.Headers;
 using System.Runtime.InteropServices;
 
 // at this point I don't know why I keep including these the book has them.. so I add them just to be sure
@@ -14,6 +16,9 @@ public class Hero : MonoBehaviour
     public float speed = 30;
     public float rollMult = -45;
     public float pitchMult = 30;
+
+    public GameObject projectilePrefab;
+    public float projectileSpeed = 40;
 
     [Header("Dynamic")] [Range(0,4)] [SerializeField]
     private float _shieldLevel = 1;
@@ -51,6 +56,20 @@ public class Hero : MonoBehaviour
         transform.position = pos;
 
         transform.rotation = Quaternion.Euler(vAxis*pitchMult,hAxis*rollMult,0);
+
+        if(Input.GetKeyDown( KeyCode.Space ) ) {
+
+            TempFire();
+
+
+        }
+    }
+
+    void TempFire() {
+        GameObject projGO = Instantiate<GameObject>( projectilePrefab );
+        projGO.transform.position = transform.position;
+        Rigidbody rigidB = projGO.GetComponent<Rigidbody>();
+        rigidB.linearVelocity =  Vector3.up * projectileSpeed;
     }
 
     void OnTriggerEnter(Collider other)
--- Assets/__Scripts/ProjectileHero.cs
+++ Assets/__Scripts/ProjectileHero.cs
@@ -1,16 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent( typeof(BoundsCheck))]
 public class ProjectileHero : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private BoundsCheck bndCheck;
+
+    void Awake()
     {
-        
+        bndCheck = GetComponent<BoundsCheck>();
     }
+
+    public Vector3 pos {
+        get {
+            return this.transform.position;
+        }
+        set {
+            this.transform.position = value;
+        }
+    }
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        if( bndCheck.LocIs(BoundsCheck.eScreenLocs.offUp)) {
+            Destroy( gameObject );
+        }
+
+        if(pos.y > 40){
+
+             Destroy( gameObject );
+
+        }
     }
 }
